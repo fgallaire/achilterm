d26c100c4e0643ead2780a7bca42d10aceb7b11c
Florent Gallaire <fgallaire@gmail.com>
2016-07-31 16:47:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 from setuptools import setup, find_packages
 
-version = '0.11'
+version = '0.15'
 
 setup(name='Achilterm',
       version=version,
       author='Florent Gallaire',
       author_email='fgallaire@gmail.com',
       url='http://fgallaire.github.io/achilterm',
-      license='GNU AGPL',
+      license='GNU AGPLv3+',
       keywords='sh shell bash',
+      classifiers=[
+          "Development Status :: 6 - Mature",
+          "Environment :: Web Environment",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "Topic :: System :: Shells",
+          "Topic :: Terminals :: Terminal Emulators/X Terminals",
+          "Programming Language :: Python :: 2.5",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+      ],
       packages=find_packages(exclude=['docs', 'examples', 'tests']),
       py_modules=['achilterm'],
       include_package_data=True,
